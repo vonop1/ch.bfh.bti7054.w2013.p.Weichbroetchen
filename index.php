4bc3e4f2e7e1ccc6b4b0bd31aa12607b245210fe
vonop1 <vonop1@bfh.ch>
2013-10-25 13:02:22 +0200

    edited design

--- index.php
+++ index.php
@@ -5,23 +5,27 @@
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
 	</head>
 	<body>
-		<div id="header">
-			<img src="images/banner.jpg" class="centerd"/>
+		<div id="wrapper">
+			<div id="header">
+				<img src="images/banner.jpg" class="centerd"/>
+			</div>
 			<div id="mainNav">
 				<?php include("html/mainNav.php");?>
 			</div>
-		</div>
-		<div id="secondNav">
-			<?php include("html/secondNav.php");?>
-		</div>
-		<div id="user">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
-		</div>
-		<div id="content">
-			<?php include("html/content.php"); ?>
-		</div>
-		<div id="footer">
-			<p>Footer</p>
-		</div>
+			<div id="contentliquid">
+				<div id="content">
+					<?php include("html/content.php"); ?>
+				</div>
+			</div>
+			<div id="secondNav">
+				<?php include("html/secondNav.php");?>
+			</div>
+			<div id="user">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+			</div>
+			<div id="footer">
+				<p>Footer</p>
+			</div>
+		<div>
 	</body>
 </html>
