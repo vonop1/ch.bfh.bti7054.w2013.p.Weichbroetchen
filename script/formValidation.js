dbc523cf302bda9e4886d907d0f366a9ed9edb58
vonop1 <vonop1@bfh.ch>
2013-11-22 11:40:17 +0100

    move registration to xml

--- script/formValidation.js
+++ script/formValidation.js
@@ -1,6 +1,5 @@
 function validateForm(){
 	var firstname = document.getElementById("firstname").value;
-	alert(firstname);
 	if (firstname == "") {
 	    //alert("Bitte Ihren Namen eingeben!");
 	    document.getElementById("firstname").focus();
