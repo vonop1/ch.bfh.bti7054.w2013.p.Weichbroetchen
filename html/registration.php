e197513ebfd6b371f85d223c04bc0213714cf527
vonop1 <vonop1@bfh.ch>
2013-11-29 11:48:55 +0100

    dual language for registration form

--- html/registration.php
+++ html/registration.php
@@ -1,54 +1,52 @@
 <?php
 	include_once ('functions.php');
-	$registrationForm = array (
-			"de" => array (
-					"firstname" => "Vorname",
-					"lastname" => "Nachname",
-					"street" => "Strasse",
-					"streetNumber" => "Hausnummer",
-					"ZIP" => "PLZ",
-					"city" => "Ort",
-					"phone" => "Telefonnummer",
-					"email" => "E-Mailadresse",
-					"emailR" => "E-Mailadresse wiederholen" 
-			),
-			"en" => array (
-					"firstname" => "Firstname",
-					"lastname" => "Lastname",
-					"street" => "Street",
-					"streetNumber" => "Streetnumber",
-					"ZIP" => "ZIP",
-					"city" => "City",
-					"phone" => "Phonenumber",
-					"email" => "E-Mailadresse",
-					"emailR" => "E-Mailadresse wiederholen" 
-			) 
-	);
 	
-	$language = get_param ( "lang", "de" );
-	$formItems = $registrationForm [$language]; // Array entsprechend der Sprache nehmen
+	//set the language and load texts for choosen language
+	$language = get_param("lang", "de");
+	$texts = simplexml_load_file("./text/$language.xml");
+	$registrationTexts = $texts->registration;
+	
+	//set title texts
+	$titleTexts = $registrationTexts->titles;
+	$mainTitle = $titleTexts->main;
+	$userRegTitle = $titleTexts->userReg;
+	$finishTitle = $titleTexts->finish;
+	
+	//set input field texts
+	$formTexts = $registrationTexts->form;
+	
+	//set button texts
+	$buttonTexts = $registrationTexts->buttons;
+	$submitValue = $buttonTexts->send;
+	$resetValue = utf8_decode($buttonTexts->reset);
+	
+	//set input field length
 	$inputSize = 50;
-	$title = "Registrierung";
-	$textReg = "Kontaktangaben";
-	$textSend = "Abschliessen";
 	
-	echo '<h2>' .$title. '</h2>';
+	//create html content
+	echo '<h2>' .$mainTitle. '</h2>';
 	echo '<form action ="evaluateReg.php" onsubmit="return validateForm()" method="post">';
 	echo '<fieldset class="registration">';
-	echo '<legend>' .$textReg. '</legend>';
+	echo '<legend>' .$userRegTitle. '</legend>';
 	$accesskey = 1;
-	foreach ( $formItems as $key => $value ) {
-		echo '<label accesskey="' .$accesskey. '" for="' .$key. '">' .$value. '</label>';
-		echo '<input id="' .$key. '" name="' .$key. '" size="' .$inputSize. '"></input>';
+	foreach ($formTexts->children() as $child){
+		$childName = $child->getName();
+		echo '<label accesskey="' .$accesskey. '" for="' .$childName. '">' .$child. '</label>';
+		if ($childName == "password" || $childName == "passwordR") {
+			echo '<input type="password" id="' .$childName. '" name="' .$childName. '" size="' .$inputSize. '"></input>';
+		}else{
+			echo '<input id="' .$childName. '" name="' .$childName. '" size="' .$inputSize. '"></input>';
+		}
 		echo '<br></br>';
 		$accesskey++;
 	}
+	
 	echo '</fieldset>';
 	echo '<br></br>';
 	echo '<fieldset class="buttons">';
-	echo '<legend>' .$textSend. '</legend>';
-	echo '<input id="submit" type="submit" value="Abschicken"></input>';
-	echo '<input id="reset" type="reset" value="Eingaben löschen"></input>';
+	echo '<legend>' .$finishTitle. '</legend>';
+	echo '<input id="submit" type="submit" value="' .$submitValue. '"></input>';
+	echo '<input id="reset" type="reset" value="' .$resetValue. '"></input>';
 	echo '</fieldset>';
 	echo '</form>';
 
