887b65f82a6e025cbf0c776e3b615c42a7682931
vonop1 <vonop1@bfh.ch>
2014-01-01 20:41:48 +0100

    revision for user links

--- html/user.php
+++ html/user.php
@@ -2,49 +2,22 @@
 include_once ('functions.php');
 
 // set the language and load texts for choosen language
-$language = get_param ( "lang", "de" );
-$texts = simplexml_load_file ( "./text/$language.xml" );
+$language = get_param ("lang", "de");
+$texts = simplexml_load_file("./text/$language.xml");
 $userTexts = $texts->user;
 
-// check if a user is logged in
+// check if a user is logged in and set user links, 100 Offset to identify non product ids
 if (isset ( $_SESSION ["user"] )) {
-	echo "<p>Hello " . $_SESSION ["user"] . "</p>";
-	$userLink = $userTexts->LogoutLink;
-} else {
-	$userLink = $userTexts->LoginLink;
+	echo '<p id="greeting">' .$userTexts->Greeting. ' <b>' .$_SESSION["user"]. '</b></p>';
+	echo '<ul>';
+	echo '<li class="user"><a href="' .changeUrl(array("idMain" => 100, "idSec" => 0)). '">' .$userTexts->LogoutLink. '</a></li>';
+	echo '<li class="user"><a href="' .changeUrl(array("idMain" => 102, "idSec" => 0)). '">' .$userTexts->Cart. '</a></li>';
+	echo '</ul>';
+}else{
+	echo '<ul>';
+	echo '<li class="user"><a href="' .changeUrl(array("idMain" => 101, "idSec" => 0)). '">' .$userTexts->RegistrationLink. '</a></li>';
+	echo '<li class="user"><a href="' .changeUrl(array("idMain" => 100, "idSec" => 0)). '">' .$userTexts->LoginLink. '</a></li>';
+	echo '</ul>';
 }
 
-echo '<ul>';
-
-// 100 Offset to identify non product ids
-echo '<li class="user">';
-echo "<a href=\"" . changeUrl ( array (
-		"idMain" => 100,
-		"idSec" => 0 
-) ) . "\">";
-echo $userLink;
-echo "</a>";
-echo '</li>';
-
-if (!isset ( $_SESSION ["user"] )) {
-	echo '<li class="user">';
-	echo "<a href=\"" . changeUrl ( array (
-			"idMain" => 101,
-			"idSec" => 0
-	) ) . "\">";
-	echo "$userTexts->RegistrationLink";
-	echo "</a>";
-	echo '</li>';
-}
-
-echo '<li class="user">';
-echo "<a href=\"" . changeUrl ( array (
-		"idMain" => 102,
-		"idSec" => 0 
-) ) . "\">";
-echo "$userTexts->Cart";
-echo "</a>";
-echo '</li>';
-
-echo '</ul>';
 ?>
